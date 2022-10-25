c092cc5226debda1b53f07ab0e1c330dc90bd0f2
Ruhaan Doshi <ruhaand@umich.edu>
2022-10-25 14:59:08 -0400

    add an endpoint for getting one club: /clubs/[club_id]

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,15 @@
-const express = require("express");
-const { createClient } = require("@supabase/supabase-js");
+const express = require('express');
+const { createClient } = require('@supabase/supabase-js');
+
 const supabase = createClient(
-  "https://mdwlzfwjihhrecmsgotj.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kd2x6ZndqaWhocmVjbXNnb3RqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2NDI5NzI1MSwiZXhwIjoxOTc5ODczMjUxfQ.8K-mtTWV_Ls8t7WK1jRj-_XHmQGAgEKfAAB2yku-eXY"
+  'https://mdwlzfwjihhrecmsgotj.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kd2x6ZndqaWhocmVjbXNnb3RqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ0NjE0ODIsImV4cCI6MTk4MDAzNzQ4Mn0.5LaklqDwL_S4jNnY5ZptTf7V68wCNFJY519sOQEwPG4',
 );
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   res.json({
-    message: "API - 👋🌎🌍🌏",
+    message: 'API - 👋🌎🌍🌏',
   });
 });
 
@@ -16,23 +17,36 @@ router.get("/getAllClubs", async (req, res, next) => {
   const { limit } = req.query; // get request params
   console.log();
   const { data, error } = await supabase
-    .from("organizations")
-    .select("*")
-    .limit(limit || 2000);
+      .from("organizations")
+      .select("*")
+      .limit(limit || 2000);
   if (error) return res.json(error);
   res.json(data);
 });
 
-router.get("/searchClubs", async (req, res, next) => {
+router.get('/searchClubs', async (req, res, next) => {
   const { query } = req.query; // get request params
   console.log(req.params, req.query);
   const { data, error } = await supabase // query db
-    .from("organizations")
-    .select("*")
+    .from('organizations')
+    .select('*')
     .or(`name.ilike.*${query}*,shortName.ilike.*${query}*,summary.ilike.*${query}*`)
     .limit(100);
   if (error) return res.json(error);
   res.json({ count: data.length, results: data });
 });
+
+router.get('/clubs/:club_id', async (req, res) => {
+  const { club_id } = req.params;
+  // TODO(ruhaan): should I be querying based on name or id
+  const { data, error } = await supabase
+      .from('organizations')
+      .select('*')
+      .eq('id', `${club_id}`)
+      .limit(1);
+  if (error) return res.json(error);
+  // console.log(data);
+  res.json(data);
+})
 
 module.exports = router;
